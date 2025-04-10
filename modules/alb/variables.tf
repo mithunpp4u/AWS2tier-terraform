534e808bdf6776c4ce27b16ad54adb75dd70c83b
Mithun <mithunpp4u@gmail.com>
2025-04-10 17:58:58 +0530

    added variables in root/main.tf for alb and asg

---
+++ modules/alb/variables.tf
@@ -0,0 +1,8 @@
+variable "project_name" {}
+variable "web_sg_id" {}
+variable "alb_sg_id" {}
+variable "pb_sub1_id" {}
+variable "pb_sub2_id" {}
+variable "pr_sub1_id" {}
+variable "pr_sub2_id" {}
+variable "vpc_id" {}
--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -1,1 +1,16 @@
 variable "project_name" {}
+variable "key_name" {}
+variable "instance_type" {}
+variable "web_ami" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "desired_cap" {}
+variable "health_check_type" {}
+
+variable "pr_sub1_id" {}
+variable "pr_sub2_id" {}
+variable "vpc_id" {}
+
+
+variable "web_sg_id" {}
+variable "alb_sg_id" {}
